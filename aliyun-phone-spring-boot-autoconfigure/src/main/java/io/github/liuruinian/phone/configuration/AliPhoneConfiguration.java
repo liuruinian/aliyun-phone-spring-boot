55bf927ad34ae77d053028d64eba1447c93d7a81
liuruinian <1394329227@qq.com>
2023-02-08 22:09:27 +0800

    axb

--- aliyun-phone-spring-boot-autoconfigure/src/main/java/io/github/liuruinian/phone/configuration/AliPhoneConfiguration.java
+++ aliyun-phone-spring-boot-autoconfigure/src/main/java/io/github/liuruinian/phone/configuration/AliPhoneConfiguration.java
@@ -4,6 +4,7 @@ import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
+import io.github.liuruinian.phone.api.axb.delegate.AxbBindDelegate;
 import io.github.liuruinian.phone.api.axn.delegate.AxnBindDelegate;
 import io.github.liuruinian.phone.api.state.delegate.StateQueryDelegate;
 import io.github.liuruinian.phone.api.subscribe.delegate.SubscriptionOperationDelegate;
@@ -43,6 +44,11 @@ public class AliPhoneConfiguration {
     @Bean
     public AxnBindDelegate axnBindDelegate(IAcsClient acsClient, AsyncThreadPoolExecutor asyncThreadPoolExecutor) {
         return new AxnBindDelegate(acsClient, asyncThreadPoolExecutor);
+    }
+
+    @Bean
+    public AxbBindDelegate axbBindDelegate(IAcsClient acsClient) {
+        return new AxbBindDelegate(acsClient);
     }
 
     @Bean
